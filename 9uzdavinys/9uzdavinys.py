013dde1cad382b4d25ea41eecc66c56e1acc5a76
Dalia Masilionyte <dmasilionyte@gmail.com>
2018-06-18 17:15:55 +0300

    reusing the code

--- 9uzdavinys/9uzdavinys.py
+++ 9uzdavinys/9uzdavinys.py
@@ -29,25 +29,12 @@ try:
 except:
     sys.exit ('Reikia įvesti dviejų failų pavadinimus')
 
-
-
-# Tranzicijos - mutacija, A<->G, C<->T 
-def countTransitions(seq1, seq2):
-    transitionCounter = 0
+def countMutations(seq1, seq2, mutationType):
+    counter = 0
     for base in range(len(seq1)):
-        if seq2[base] == transitions[seq1[base]]:
-            transitionCounter += 1
-    return transitionCounter
-
-# Transvercija - mutacija, A<->C, G<->T A<->T, C<->G
-def countTransversion(seq1, seq2):
-    transversionCounter = 0
-    for base in range(len(seq1)):
-        if seq2[base] in transversions[seq1[base]]:
-            transversionCounter += 1
-    return transversionCounter
-
-
+        if seq2[base] in mutationType[seq1[base]]:
+            counter += 1
+    return counter
 
 try:
     with open(inputFile, 'r', encoding = 'utf-8') as input:
@@ -56,8 +43,8 @@ try:
             sequence = match.group(1).replace('\n', '')
             sequences.append(sequence)
 
-        transit = countTransitions(sequences[0], sequences[1])
-        transv = countTransversion(sequences[0], sequences[1])
+        transit = countMutations(sequences[0], sequences[1], transitions)
+        transv = countMutations(sequences[0], sequences[1], transversions)
         ratio = float(transit/transv)
 
 
